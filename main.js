cd63e360c8a9166ffbea62ec39a91831300250b3
nikhilpn725 <nikhilpn725@gmail.com>
2023-02-02 16:08:30 +0530

    typewritter effect bug fixed

--- main.js
+++ main.js
@@ -1,44 +1,35 @@
-let list = ["BlockChain Developer", "SDE", "EnterPrenuer"]
-let typeWriter =  document.getElementById("dynamic-content")
+let list = ["BlockChain Developer", "SDE", "EnterPrenuer"];
+let typeWriter = document.getElementById("dynamic-content");
 
-let letterIndex = 0
-let phraseIndex = 0
+let letterIndex = 0;
+let phraseIndex = 0;
 
-function printLetter(list){
-    if (letterIndex == list.length){
-        clearLetter()
-
-    }
-    else if (letterIndex < list.length){
-        typeWriter.textContent += list.charAt(letterIndex)
-        letterIndex +=1;
-        setTimeout(function(){
-            printLetter(list)
-        },300)
-
-    }
+function printLetter(list) {
+  if (letterIndex == list.length) {
+    clearLetter();
+  } else if (letterIndex < list.length) {
+    typeWriter.textContent += list.charAt(letterIndex);
+    letterIndex += 1;
+    setTimeout(function () {
+      printLetter(list);
+    }, 300);
+  }
 }
 
-function clearLetter(){
-    if (letterIndex == -1){
-        phraseIndex = (phraseIndex +1) % list.length
-        letterIndex = 0 ;
-        printLetter(list[phraseIndex])
-    }
-
-    else if (letterIndex > -1){
+function clearLetter() {
+  if (letterIndex == -1) {
+    phraseIndex = (phraseIndex + 1) % list.length;
+    letterIndex = 0;
+    printLetter(list[phraseIndex]);
+  } else if (letterIndex > -1) {
     let updated = "";
-    for (let index = 0; index < letterIndex; index++){
-        updated += list[phraseIndex].charAt(index)
+    for (let index = 0; index < letterIndex; index++) {
+      updated += list[phraseIndex].charAt(index);
     }
-    typeWriter.textContent= updated;
-    letterIndex -=1
-    setTimeout(clearLetter, 100)
-
-   }
-
+    typeWriter.textContent = updated;
+    letterIndex -= 1;
+    setTimeout(clearLetter, 100);
+  }
 }
 
-printLetter(list[phraseIndex])
-
-
+printLetter(list[phraseIndex]);
