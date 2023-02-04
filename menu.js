edcc3531c50fe6fedf5fbac9d1850933048e2be7
nikhilpn725 <nikhilpn725@gmail.com>
2023-02-04 14:56:15 +0530

    click outside the m-nav close the nav fun added

--- menu.js
+++ menu.js
@@ -1,7 +1,9 @@
 const menuButton = document.querySelector("#menu");
 const mobileNav = document.querySelector("#mobile-nav");
+const closeButton = document.querySelector("  #close-button");
 
-menuButton.addEventListener("click", () => {
+menuButton.addEventListener("click", (event) => {
+  event.stopPropagation();
   console.log("click");
   console.log(mobileNav.style.display);
   if (mobileNav.style.display == "") {
@@ -10,5 +12,16 @@ menuButton.addEventListener("click", () => {
     mobileNav.style.display = "none";
   } else {
     mobileNav.style.display = "block";
+  }
+});
+closeButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  mobileNav.style.display = "none";
+});
+
+document.addEventListener("click", (event) => {
+  if (!mobileNav.contains(event.target)) {
+    mobileNav.style.display = "none";
+    console.log("reache here");
   }
 });
